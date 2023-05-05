b3f8e68139b0872346f52ffa1f65d6f231962a4d
biber <dlehdxo2@gmail.com>
2023-05-06 01:09:17 +0900

    📝 : 추가 SqlException
    message 만 넣을 수 있는 생성자 추가

--- src/main/java/com/zerobase/zerobasestudy/util/exception/SqlException.java
+++ src/main/java/com/zerobase/zerobasestudy/util/exception/SqlException.java
@@ -2,6 +2,11 @@ package com.zerobase.zerobasestudy.util.exception;
 
 public class SqlException extends RuntimeException{
 
+
+    public SqlException(String message) {
+        super(message);
+    }
+
     public SqlException(String message, Throwable cause) {
         super(message, cause);
     }
