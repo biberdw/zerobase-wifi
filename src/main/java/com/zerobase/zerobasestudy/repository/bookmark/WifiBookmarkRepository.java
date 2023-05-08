05dc5ba30ca1ba87f30fff442e835eb18e0aea99
biber <dlehdxo2@gmail.com>
2023-05-08 19:03:56 +0900

    ✨ : 추가 삭제 , 수정
    부모테이블 삭제 및 수정시 자식테이블 삭제 및 수정

--- src/main/java/com/zerobase/zerobasestudy/repository/bookmark/WifiBookmarkRepository.java
+++ src/main/java/com/zerobase/zerobasestudy/repository/bookmark/WifiBookmarkRepository.java
@@ -15,4 +15,16 @@ public interface WifiBookmarkRepository {
 
     /** 와이파이 삭제 */
     int deleteById(Long wifiId, Long bookmarkId);
+
+    /** 즐겨찾기와 같은 와이파이_즐겨찾기 조회 */
+    List<WifiBookmark> findAllByBookmarkId(Long bookmarkId);
+
+    /** 즐겨찾기 이름 변경 */
+    int updateBookmarkNameByBookmarkId(String name, Long bookmarkId);
+
+    /** 즐겨찾기와 같은 와이파이_즐겨찾기 삭제 */
+    int deleteByBookmarkId(Long bookmarkId);
+
+    /** 와이파이_즐겨찾기 전체삭제 */
+    int deleteAll();
 }
